01ea40d261e96de90c679a423977158e57f73e7f
XZEERH <razzyr408@gmail.com>
2026-07-03 19:53:55 +0700

    Update video-helper.ts

--- utils/video-helper.ts
+++ utils/video-helper.ts
@@ -1,8 +1,18 @@
-export const getVideoDuration = (file: File): Promise<number> => {
+/**
+ * Mendapatkan durasi video
+ */
+export const getVideoMetadata = (file: File): Promise<{ duration: string; resolution: string }> => {
   return new Promise((resolve) => {
     const video = document.createElement('video');
     video.preload = 'metadata';
+    video.onloadedmetadata = () => {
+      window.URL.revokeObjectURL(video.src);
+      const dur = Math.round(video.duration);
+      resolve({
+        duration: `${dur}s`,
+        resolution: `${video.videoWidth}x${video.videoHeight}`
+      });
+    };
     video.src = URL.createObjectURL(file);
-    video.onloadedmetadata = () => resolve(video.duration);
   });
 };
