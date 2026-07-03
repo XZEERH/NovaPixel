c7ae43146467692def179908da9f731211c21538
XZEERH <razzyr408@gmail.com>
2026-07-04 03:17:41 +0700

    Update GlassCard.tsx

--- components/UI/GlassCard.tsx
+++ components/UI/GlassCard.tsx
@@ -17,4 +17,4 @@ const GlassCard = ({ children, className = "" }: GlassCardProps) => (
   </motion.div>
 );
 
-export default GlassCard; // Export Default
+export default GlassCard; // WAJIB DEFAULT EXPORT
