8e0542f0e5e1242804e3f82b7b48ac3415e067c0
XZEERH <razzyr408@gmail.com>
2026-07-04 02:53:52 +0700

    Update api-service.ts

--- services/api-service.ts
+++ services/api-service.ts
@@ -1,8 +1,7 @@
-import apiClient from '@/lib/axios-instance';
+import apiClient from '@/lib/axios-instance'; // GUNAKAN HURUF KECIL
 import { AIResponse } from '@/types';
 
 export const processMedia = async (url: string, type: 'image' | 'video'): Promise<string> => {
-  // Kita gunakan API internal route untuk menghindari CORS
   const { data } = await apiClient.get<AIResponse>(`/api/proxy`, {
     params: { url, type }
   });
