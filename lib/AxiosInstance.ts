e74a8b509d8cda893dc28a0bfe941bdd8c1470e7
XZEERH <razzyr408@gmail.com>
2026-07-04 02:33:42 +0700

    Create AxiosInstance.ts

---
+++ lib/AxiosInstance.ts
@@ -0,0 +1,11 @@
+// lib/AxiosInstance.ts
+import axios from 'axios';
+
+const apiClient = axios.create({
+  timeout: 300000,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+export default apiClient;
